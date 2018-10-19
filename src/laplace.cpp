331db28e45e3287e5f93e6c5407d926f9d7269d8
Ahmed Elket <ahmedelket@rio.gsic.titech.ac.jp>
2018-10-19 16:06:22 +0900

    p2p gpu working

--- src/laplace.cpp
+++ src/laplace.cpp
@@ -48,6 +48,51 @@ namespace exafmm_t {
     return temp;
   }
 
+void P2PKernel_test(real_t* trg_coord, real_t* trg_value, real_t* src_coord, real_t* src_value, int leafs_cnt, int ncrit, int adj_cnt) {
+  int src_cnt = ncrit*adj_cnt;
+  const real_t COEFP = 1.0/(2*4*M_PI);  
+  const real_t COEFG = -1.0/(4*2*2*6*M_PI);
+  for (int i=0;i<leafs_cnt;i++) {
+    for (int j = 0; j < ncrit; j++)
+    {
+      int idx = i * ncrit + j;
+      real_t tx = trg_coord[3*idx+0];
+      real_t ty = trg_coord[3*idx+1];
+      real_t tz = trg_coord[3*idx+2];
+      real_t tv0=0;
+      real_t tv1=0;
+      real_t tv2=0;
+      real_t tv3=0;
+      
+      for(int k=i*src_cnt; k<i*src_cnt+src_cnt; k++) {
+        real_t sx = src_coord[3*k+0] - tx;
+        real_t sy = src_coord[3*k+1] - ty;
+        real_t sz = src_coord[3*k+2] - tz;
+        real_t r2 = sx*sx + sy*sy + sz*sz;
+        real_t sv = src_value[k];
+        if (r2 != 0)
+        {
+          real_t invR = 1.0/sqrt(r2);
+          real_t invR3 = invR*invR*invR;
+          tv0 += invR*sv;
+          sv *= invR3;
+          tv1 += sv*sx;
+          tv2 += sv*sy;
+          tv3 += sv*sz;
+        }
+      }
+      tv0 *= COEFP;
+    tv1 *= COEFG;
+    tv2 *= COEFG;
+    tv3 *= COEFG;
+    trg_value[4*idx+0] += tv0;  
+    trg_value[4*idx+1] += tv1;
+    trg_value[4*idx+2] += tv2;
+    trg_value[4*idx+3] += tv3;
+    }
+  } 
+}
+
   void potentialP2P(RealVec& src_coord, RealVec& src_value, RealVec& trg_coord, RealVec& trg_value) {
     simdvec zero((real_t)0);
     const real_t COEF = 1.0/(2*4*M_PI);   // factor 16 comes from the simd rsqrt function
@@ -83,58 +128,50 @@ namespace exafmm_t {
     //Profile::Add_FLOP((long long)trg_cnt*(long long)src_cnt*20);
   }
 
-  void gradientP2P(RealVec& src_coord, RealVec& src_value, RealVec& trg_coord, RealVec& trg_value) {
-    simdvec zero((real_t)0);
-    const real_t COEFP = 1.0/(2*4*M_PI);   // factor 16 comes from the simd rsqrt function
+void gradientP2P(RealVec& src_coord, RealVec& src_value, 
+    RealVec& trg_coord, RealVec& trg_value) {
+	//std::cout<<"src_coord  src_val  "<<src_coord.size()<<"  "<<src_value.size()<<std::endl;
+	const real_t COEFP = 1.0/(2*4*M_PI);   // factor 16 comes from the simd rsqrt function
     const real_t COEFG = -1.0/(4*2*2*6*M_PI);
-    simdvec coefp(COEFP);
-    simdvec coefg(COEFG);
-    int src_cnt = src_coord.size() / 3;
+	int src_cnt = src_coord.size() / 3;
     int trg_cnt = trg_coord.size() / 3;
-    for(int t=0; t<trg_cnt; t+=NSIMD) {
-      simdvec tx(&trg_coord[3*t+0], 3*(int)sizeof(real_t));
-      simdvec ty(&trg_coord[3*t+1], 3*(int)sizeof(real_t));
-      simdvec tz(&trg_coord[3*t+2], 3*(int)sizeof(real_t));
-      simdvec tv0(zero);
-      simdvec tv1(zero);
-      simdvec tv2(zero);
-      simdvec tv3(zero);
-      for(int s=0; s<src_cnt; s++) {
-        simdvec sx(src_coord[3*s+0]);
-        sx = tx - sx;
-        simdvec sy(src_coord[3*s+1]);
-        sy = ty - sy;
-        simdvec sz(src_coord[3*s+2]);
-        sz = tz - sz;
-        simdvec r2(zero);
-        r2 += sx * sx;
-        r2 += sy * sy;
-        r2 += sz * sz;
-        simdvec invR = rsqrt(r2);
-        invR &= r2 > zero;
-        simdvec invR3 = (invR*invR) * invR;
-        simdvec sv(src_value[s]);
-        tv0 += sv*invR;
-        sv *= invR3;
-        tv1 += sv*sx;
-        tv2 += sv*sy;
-        tv3 += sv*sz;
-      }
-      tv0 *= coefp;
-      tv1 *= coefg;
-      tv2 *= coefg;
-      tv3 *= coefg;
-      for(int k=0; k<NSIMD && t+k<trg_cnt; k++) {
-        trg_value[0+4*(t+k)] += tv0[k];
-        trg_value[1+4*(t+k)] += tv1[k];
-        trg_value[2+4*(t+k)] += tv2[k];
-        trg_value[3+4*(t+k)] += tv3[k];
-      }
+    for(int i=0; i<trg_cnt; i++) {
+    	real_t tx = trg_coord[3*i+0];
+    	real_t ty = trg_coord[3*i+1];
+    	real_t tz = trg_coord[3*i+2];
+    	real_t tv0=0;
+    	real_t tv1=0;
+    	real_t tv2=0;
+    	real_t tv3=0;
+    	for(int j=0; j<src_cnt; j++) {
+    		real_t sx = src_coord[3*j+0] - tx;
+	        real_t sy = src_coord[3*j+1] - ty;
+	        real_t sz = src_coord[3*j+2] - tz;
+	        real_t r2 = sx*sx + sy*sy + sz*sz;	
+	        real_t sv = src_value[j];
+	        if (r2 != 0)
+	        {
+	          	real_t invR = 1.0/sqrt(r2);
+	          	real_t invR3 = invR*invR*invR;
+	          	tv0 += invR*sv;
+	          	sv *= invR3;
+	          	tv1 += sv*sx;
+        	   	tv2 += sv*sy;
+        		tv3 += sv*sz;
+	        }
+		}
+		tv0 *= COEFP;
+		tv1 *= COEFG;
+      	tv2 *= COEFG;
+      	tv3 *= COEFG;
+    	trg_value[4*i+0] += tv0;
+    	trg_value[4*i+1] += tv1;
+    	trg_value[4*i+2] += tv2;
+    	trg_value[4*i+3] += tv3;
     }
-    //Profile::Add_FLOP((long long)trg_cnt*(long long)src_cnt*27);
-  }
+}
 
-  //! Laplace P2P save pairwise contributions to k_out (not aggregate over each target)
+//! Laplace P2P save pairwise contributions to k_out (not aggregate over each target)
   void kernelMatrix(real_t* r_src, int src_cnt, real_t* r_trg, int trg_cnt, real_t* k_out) {
     RealVec src_value(1, 1.);
     RealVec trg_coord(r_trg, r_trg+3*trg_cnt);
@@ -308,39 +345,40 @@ namespace exafmm_t {
     int coord_count = ncrit*3;
     int src_box_count = 27;
     real_t *trg_pt_coord=new real_t[targets.size()*coord_count]();
-    real_t *trg_pt_trg=new real_t[targets.size()*ncrit]();
+    real_t *trg_pt_trg=new real_t[targets.size()*ncrit*4]();
     real_t *src_pt_coord = new real_t[targets.size()*src_box_count*coord_count]();
     real_t *src_pt_src = new real_t[targets.size()*src_box_count*ncrit]();
-    int trg_coord_idx = 0;
-    int src_coord_idx = 0;
-    int val_idx = 0;
-
     for(int i=0; i<targets.size(); i++) {
       Node* target = targets[i];
       std::vector<Node*>& sources = target->P2Plist;
+      RealVec& trg_coord = target->pt_coord;
+      RealVec& trg_val = target->pt_trg;
+
+      for (int k = 0; k < trg_coord.size(); k++)
+        trg_pt_coord[i*ncrit*3+k] = trg_coord[k];
+      for (int k = 0; k < trg_val.size(); k++)
+        trg_pt_trg[i*ncrit*4+k] = trg_val[k];
+
       for(int j=0; j<sources.size(); j++) {
         Node* source = sources[j];
-	RealVec& src_coord = source->pt_coord; 
-        std::copy(src_coord.begin(), src_coord.end(), src_pt_coord+src_coord_idx);
-	src_coord_idx += coord_count;
-	RealVec& pt_src = source->pt_src;
-	std::copy(pt_src.begin(), pt_src.end(), src_pt_src+val_idx);
-	val_idx += ncrit;
 	//gradientP2P(source->pt_coord, source->pt_src, target->pt_coord, target->pt_trg);
+	RealVec& src_coord = source->pt_coord; 
+	RealVec& src_val = source->pt_src;
+
+	for (int k = 0; k < src_coord.size(); k++)
+           src_pt_coord[i*src_box_count*ncrit*3+(j*ncrit*3)+k] = src_coord[k];
+      for (int k = 0; k < src_val.size(); k++) 
+          src_pt_src[i*src_box_count*ncrit+(j*ncrit)+k] = src_val[k];
       }
-      src_coord_idx += (src_box_count-sources.size())*coord_count;
-      val_idx += (src_box_count-sources.size())*ncrit;
-      RealVec& trg_coord = target->pt_coord;
-      std::copy(trg_coord.begin(), trg_coord.end(), trg_pt_coord+trg_coord_idx);
-      trg_coord_idx += coord_count;
-    }
+   }
     P2PGPU(trg_pt_coord, trg_pt_trg, src_pt_coord, src_pt_src, targets.size(), ncrit, src_box_count);
-    
+//	P2PKernel_test(trg_pt_coord, trg_pt_trg, src_pt_coord, src_pt_src, targets.size(), ncrit, src_box_count);
+
     for(int i=0; i<targets.size(); i++) {
       Node* target = targets[i];
       RealVec& trg_val = target->pt_trg;
      for(int j=0;j<trg_val.size();j++) {
-        trg_val[j] = trg_pt_trg[i*ncrit+j];
+        trg_val[j] = trg_pt_trg[i*ncrit*4+j];
      }
     }
 
--- src/laplace_cuda.cu
+++ src/laplace_cuda.cu
@@ -14,14 +14,19 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 namespace exafmm_t {
 __global__ 
-void potentialP2PKernel(real_t *src_coord, real_t *src_value, real_t *trg_coord, real_t *trg_value, int src_cnt) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;    
-    const real_t COEF = 1.0/(2*4*M_PI);
+void potentialP2PKernel(real_t *src_coord, real_t *src_value, real_t *trg_coord, real_t *trg_value, int adj_cnt) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int src_cnt = 64*27;
+    const real_t COEFP = 1.0/(2*4*M_PI);
+  const real_t COEFG = -1.0/(4*2*2*6*M_PI);
     real_t tx = trg_coord[3*i+0];
     real_t ty = trg_coord[3*i+1];
     real_t tz = trg_coord[3*i+2];
-    real_t tv = 0;
-    for(int j=0; j<src_cnt; j++) {
+    real_t tv0=0;
+      real_t tv1=0;
+      real_t tv2=0;
+      real_t tv3=0;
+    for(int j=blockIdx.x*src_cnt; j<blockIdx.x*src_cnt+src_cnt; j++) {
     	real_t sx = src_coord[3*j+0] - tx;
         real_t sy = src_coord[3*j+1] - ty;
         real_t sz = src_coord[3*j+2] - tz;
@@ -30,29 +35,40 @@ void potentialP2PKernel(real_t *src_coord, real_t *src_value, real_t *trg_coord,
         if (r2 != 0)
         {
           real_t invR = 1.0/sqrt(r2);
-          tv += invR*sv;
+          real_t invR3 = invR*invR*invR;
+          tv0 += invR*sv;
+          sv *= invR3;
+          tv1 += sv*sx;
+          tv2 += sv*sy;
+          tv3 += sv*sz;
         }
     }
-    tv *= COEF;
-    trg_value[i] += tv;
+    tv0 *= COEFP;
+    tv1 *= COEFG;
+    tv2 *= COEFG;
+    tv3 *= COEFG;
+    trg_value[4*i+0] += tv0;
+    trg_value[4*i+1] += tv1;
+    trg_value[4*i+2] += tv2;
+    trg_value[4*i+3] += tv3;
 }
 
-void P2PGPU(real_t* trg_coord, real_t* trg_val, real_t* src_coord, real_t* src_val, int leafs_cnt, int ncrit, int adj_cnt) {
-    int THREADS = ncrit;
+void P2PGPU(real_t* trg_coord, real_t* trg_value, real_t* src_coord, real_t* src_value, int leafs_cnt, int ncrit, int adj_cnt) {
+int THREADS = ncrit;
     int BLOCKS = leafs_cnt;
     real_t *src_coord_device, *src_value_device, *trg_coord_device, *trg_value_device;
     
     cudaMalloc(&src_coord_device,sizeof(real_t)*leafs_cnt*ncrit*3*adj_cnt);
     cudaMalloc(&src_value_device, sizeof(real_t)*leafs_cnt*ncrit*adj_cnt);
     cudaMalloc(&trg_coord_device,sizeof(real_t)*leafs_cnt*ncrit*3);
-    cudaMalloc(&trg_value_device, sizeof(real_t)*leafs_cnt*ncrit);
+    cudaMalloc(&trg_value_device, sizeof(real_t)*leafs_cnt*ncrit*4);
     
     cudaMemcpy(src_coord_device, src_coord, sizeof(real_t)*leafs_cnt*ncrit*3*adj_cnt, cudaMemcpyHostToDevice);
-    cudaMemcpy(src_value_device, src_val, sizeof(real_t)*leafs_cnt*ncrit*adj_cnt, cudaMemcpyHostToDevice);
+    cudaMemcpy(src_value_device, src_value, sizeof(real_t)*leafs_cnt*ncrit*adj_cnt, cudaMemcpyHostToDevice);
     cudaMemcpy(trg_coord_device, trg_coord, sizeof(real_t)*leafs_cnt*ncrit*3, cudaMemcpyHostToDevice);
-    cudaMemcpy(trg_value_device, trg_val, sizeof(real_t)*leafs_cnt*ncrit, cudaMemcpyHostToDevice);
-    potentialP2PKernel<<<BLOCKS,THREADS>>>(src_coord_device, src_value_device, trg_coord_device, trg_value_device,ncrit*adj_cnt*3);
-    cudaMemcpy(trg_val, trg_value_device, sizeof(real_t)*leafs_cnt*ncrit, cudaMemcpyDeviceToHost);
+    cudaMemcpy(trg_value_device, trg_value, sizeof(real_t)*leafs_cnt*4*ncrit, cudaMemcpyHostToDevice);
+    potentialP2PKernel<<<BLOCKS,THREADS>>>(src_coord_device, src_value_device, trg_coord_device, trg_value_device,adj_cnt);
+    cudaMemcpy(trg_value, trg_value_device, sizeof(real_t)*leafs_cnt*4*ncrit, cudaMemcpyDeviceToHost);
  
   }
 }
