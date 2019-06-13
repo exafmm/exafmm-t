2dd7a59e36ba1f49b8e67b26a80957af5ddd8252
Tingyu Wang <tingyu.wang6@gmail.com>
2019-06-13 15:08:31 -0400

    fix intrinsics.h for FLOAT

--- include/intrinsics.h
+++ include/intrinsics.h
@@ -2,6 +2,102 @@
 #define intrinsics_h
 
 namespace exafmm_t {
+#if FLOAT
+  inline void matmult_8x8x2(float*& M_, float*& IN0, float*& IN1, float*& OUT0, float*& OUT1){
+    __m128 out00,out01,out10,out11;
+    __m128 out20,out21,out30,out31;
+    float* in0__ = IN0;
+    float* in1__ = IN1;
+    out00 = _mm_load_ps(OUT0);
+    out01 = _mm_load_ps(OUT1);
+    out10 = _mm_load_ps(OUT0+4);
+    out11 = _mm_load_ps(OUT1+4);
+    out20 = _mm_load_ps(OUT0+8);
+    out21 = _mm_load_ps(OUT1+8);
+    out30 = _mm_load_ps(OUT0+12);
+    out31 = _mm_load_ps(OUT1+12);
+    for(int i2=0;i2<8;i2+=2){
+      __m128 m00;
+      __m128 mt0,mtt0;
+      __m128 in00,in00_r,in01,in01_r;
+      in00 = _mm_castpd_ps(_mm_load_pd1((const double*)in0__));
+      in00_r = _mm_shuffle_ps(in00,in00,_MM_SHUFFLE(2,3,0,1));
+      in01 = _mm_castpd_ps(_mm_load_pd1((const double*)in1__));
+      in01_r = _mm_shuffle_ps(in01,in01,_MM_SHUFFLE(2,3,0,1));
+      m00 = _mm_load_ps(M_);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out00= _mm_add_ps   (out00,_mm_mul_ps( mt0,in00  ));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out00= _mm_addsub_ps(out00,_mm_mul_ps(mtt0,in00_r));
+      out01 = _mm_add_ps   (out01,_mm_mul_ps( mt0,in01  ));
+      out01 = _mm_addsub_ps(out01,_mm_mul_ps(mtt0,in01_r));
+      m00 = _mm_load_ps(M_+4);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out10= _mm_add_ps   (out10,_mm_mul_ps( mt0,in00  ));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out10= _mm_addsub_ps(out10,_mm_mul_ps(mtt0,in00_r));
+      out11 = _mm_add_ps   (out11,_mm_mul_ps( mt0,in01  ));
+      out11 = _mm_addsub_ps(out11,_mm_mul_ps(mtt0,in01_r));
+      m00 = _mm_load_ps(M_+8);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out20= _mm_add_ps   (out20,_mm_mul_ps( mt0,in00  ));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out20= _mm_addsub_ps(out20,_mm_mul_ps(mtt0,in00_r));
+      out21 = _mm_add_ps   (out21,_mm_mul_ps( mt0,in01  ));
+      out21 = _mm_addsub_ps(out21,_mm_mul_ps(mtt0,in01_r));
+      m00 = _mm_load_ps(M_+12);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out30= _mm_add_ps   (out30,_mm_mul_ps( mt0,  in00));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out30= _mm_addsub_ps(out30,_mm_mul_ps(mtt0,in00_r));
+      out31 = _mm_add_ps   (out31,_mm_mul_ps( mt0,in01  ));
+      out31 = _mm_addsub_ps(out31,_mm_mul_ps(mtt0,in01_r));
+      in00 = _mm_castpd_ps(_mm_load_pd1((const double*) (in0__+2)));
+      in00_r = _mm_shuffle_ps(in00,in00,_MM_SHUFFLE(2,3,0,1));
+      in01 = _mm_castpd_ps(_mm_load_pd1((const double*) (in1__+2)));
+      in01_r = _mm_shuffle_ps(in01,in01,_MM_SHUFFLE(2,3,0,1));
+      m00 = _mm_load_ps(M_+16);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out00= _mm_add_ps   (out00,_mm_mul_ps( mt0,in00  ));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out00= _mm_addsub_ps(out00,_mm_mul_ps(mtt0,in00_r));
+      out01 = _mm_add_ps   (out01,_mm_mul_ps( mt0,in01  ));
+      out01 = _mm_addsub_ps(out01,_mm_mul_ps(mtt0,in01_r));
+      m00 = _mm_load_ps(M_+20);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out10= _mm_add_ps   (out10,_mm_mul_ps( mt0,in00  ));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out10= _mm_addsub_ps(out10,_mm_mul_ps(mtt0,in00_r));
+      out11 = _mm_add_ps   (out11,_mm_mul_ps( mt0,in01 ));
+      out11 = _mm_addsub_ps(out11,_mm_mul_ps(mtt0,in01_r));
+      m00 = _mm_load_ps(M_+24);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out20= _mm_add_ps   (out20,_mm_mul_ps( mt0,in00  ));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out20= _mm_addsub_ps(out20,_mm_mul_ps(mtt0,in00_r));
+      out21 = _mm_add_ps   (out21,_mm_mul_ps( mt0,in01  ));
+      out21 = _mm_addsub_ps(out21,_mm_mul_ps(mtt0,in01_r));
+      m00 = _mm_load_ps(M_+28);
+      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
+      out30= _mm_add_ps   (out30,_mm_mul_ps( mt0,in00  ));
+      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
+      out30= _mm_addsub_ps(out30,_mm_mul_ps(mtt0,in00_r));
+      out31 = _mm_add_ps   (out31,_mm_mul_ps( mt0,in01  ));
+      out31 = _mm_addsub_ps(out31,_mm_mul_ps(mtt0,in01_r));
+      M_ += 32;
+      in0__ += 4;
+      in1__ += 4;
+    }
+    _mm_store_ps(OUT0,out00);
+    _mm_store_ps(OUT1,out01);
+    _mm_store_ps(OUT0+4,out10);
+    _mm_store_ps(OUT1+4,out11);
+    _mm_store_ps(OUT0+8,out20);
+    _mm_store_ps(OUT1+8,out21);
+    _mm_store_ps(OUT0+12,out30);
+    _mm_store_ps(OUT1+12,out31);
+  }
+#else
 #ifdef __AVX512F__
   inline void matmult_8x8x2(double*& M_, double*& IN0, double*& IN1, double*& OUT0, double*& OUT1){
     double* in0_ = IN0;
@@ -173,101 +269,6 @@ namespace exafmm_t {
     _mm256_store_pd(OUT0+12,out30);
     _mm256_store_pd(OUT1+12,out31);
   }
-#elif __SSE3__
-  inline void matmult_8x8x2(float*& M_, float*& IN0, float*& IN1, float*& OUT0, float*& OUT1){
-    __m128 out00,out01,out10,out11;
-    __m128 out20,out21,out30,out31;
-    float* in0__ = IN0;
-    float* in1__ = IN1;
-    out00 = _mm_load_ps(OUT0);
-    out01 = _mm_load_ps(OUT1);
-    out10 = _mm_load_ps(OUT0+4);
-    out11 = _mm_load_ps(OUT1+4);
-    out20 = _mm_load_ps(OUT0+8);
-    out21 = _mm_load_ps(OUT1+8);
-    out30 = _mm_load_ps(OUT0+12);
-    out31 = _mm_load_ps(OUT1+12);
-    for(int i2=0;i2<8;i2+=2){
-      __m128 m00;
-      __m128 mt0,mtt0;
-      __m128 in00,in00_r,in01,in01_r;
-      in00 = _mm_castpd_ps(_mm_load_pd1((const double*)in0__));
-      in00_r = _mm_shuffle_ps(in00,in00,_MM_SHUFFLE(2,3,0,1));
-      in01 = _mm_castpd_ps(_mm_load_pd1((const double*)in1__));
-      in01_r = _mm_shuffle_ps(in01,in01,_MM_SHUFFLE(2,3,0,1));
-      m00 = _mm_load_ps(M_);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out00= _mm_add_ps   (out00,_mm_mul_ps( mt0,in00  ));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out00= _mm_addsub_ps(out00,_mm_mul_ps(mtt0,in00_r));
-      out01 = _mm_add_ps   (out01,_mm_mul_ps( mt0,in01  ));
-      out01 = _mm_addsub_ps(out01,_mm_mul_ps(mtt0,in01_r));
-      m00 = _mm_load_ps(M_+4);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out10= _mm_add_ps   (out10,_mm_mul_ps( mt0,in00  ));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out10= _mm_addsub_ps(out10,_mm_mul_ps(mtt0,in00_r));
-      out11 = _mm_add_ps   (out11,_mm_mul_ps( mt0,in01  ));
-      out11 = _mm_addsub_ps(out11,_mm_mul_ps(mtt0,in01_r));
-      m00 = _mm_load_ps(M_+8);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out20= _mm_add_ps   (out20,_mm_mul_ps( mt0,in00  ));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out20= _mm_addsub_ps(out20,_mm_mul_ps(mtt0,in00_r));
-      out21 = _mm_add_ps   (out21,_mm_mul_ps( mt0,in01  ));
-      out21 = _mm_addsub_ps(out21,_mm_mul_ps(mtt0,in01_r));
-      m00 = _mm_load_ps(M_+12);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out30= _mm_add_ps   (out30,_mm_mul_ps( mt0,  in00));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out30= _mm_addsub_ps(out30,_mm_mul_ps(mtt0,in00_r));
-      out31 = _mm_add_ps   (out31,_mm_mul_ps( mt0,in01  ));
-      out31 = _mm_addsub_ps(out31,_mm_mul_ps(mtt0,in01_r));
-      in00 = _mm_castpd_ps(_mm_load_pd1((const double*) (in0__+2)));
-      in00_r = _mm_shuffle_ps(in00,in00,_MM_SHUFFLE(2,3,0,1));
-      in01 = _mm_castpd_ps(_mm_load_pd1((const double*) (in1__+2)));
-      in01_r = _mm_shuffle_ps(in01,in01,_MM_SHUFFLE(2,3,0,1));
-      m00 = _mm_load_ps(M_+16);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out00= _mm_add_ps   (out00,_mm_mul_ps( mt0,in00  ));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out00= _mm_addsub_ps(out00,_mm_mul_ps(mtt0,in00_r));
-      out01 = _mm_add_ps   (out01,_mm_mul_ps( mt0,in01  ));
-      out01 = _mm_addsub_ps(out01,_mm_mul_ps(mtt0,in01_r));
-      m00 = _mm_load_ps(M_+20);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out10= _mm_add_ps   (out10,_mm_mul_ps( mt0,in00  ));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out10= _mm_addsub_ps(out10,_mm_mul_ps(mtt0,in00_r));
-      out11 = _mm_add_ps   (out11,_mm_mul_ps( mt0,in01 ));
-      out11 = _mm_addsub_ps(out11,_mm_mul_ps(mtt0,in01_r));
-      m00 = _mm_load_ps(M_+24);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out20= _mm_add_ps   (out20,_mm_mul_ps( mt0,in00  ));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out20= _mm_addsub_ps(out20,_mm_mul_ps(mtt0,in00_r));
-      out21 = _mm_add_ps   (out21,_mm_mul_ps( mt0,in01  ));
-      out21 = _mm_addsub_ps(out21,_mm_mul_ps(mtt0,in01_r));
-      m00 = _mm_load_ps(M_+28);
-      mt0  = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(2,2,0,0));
-      out30= _mm_add_ps   (out30,_mm_mul_ps( mt0,in00  ));
-      mtt0 = _mm_shuffle_ps(m00,m00,_MM_SHUFFLE(3,3,1,1));
-      out30= _mm_addsub_ps(out30,_mm_mul_ps(mtt0,in00_r));
-      out31 = _mm_add_ps   (out31,_mm_mul_ps( mt0,in01  ));
-      out31 = _mm_addsub_ps(out31,_mm_mul_ps(mtt0,in01_r));
-      M_ += 32;
-      in0__ += 4;
-      in1__ += 4;
-    }
-    _mm_store_ps(OUT0,out00);
-    _mm_store_ps(OUT1,out01);
-    _mm_store_ps(OUT0+4,out10);
-    _mm_store_ps(OUT1+4,out11);
-    _mm_store_ps(OUT0+8,out20);
-    _mm_store_ps(OUT1+8,out21);
-    _mm_store_ps(OUT0+12,out30);
-    _mm_store_ps(OUT1+12,out31);
-  }
 #else
   inline void matmult_8x8x2(real_t*& M_, real_t*& IN0, real_t*& IN1, real_t*& OUT0, real_t*& OUT1){
     // Generic code.
@@ -331,6 +332,7 @@ namespace exafmm_t {
       OUT1+=4;
     }
   }
+#endif
 #endif
 }
 #endif
