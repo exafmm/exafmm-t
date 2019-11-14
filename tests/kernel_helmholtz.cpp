1c8b81489992c17569e13c77503c31eec96219d0
Tingyu Wang <tingyu.wang6@gmail.com>
2019-11-14 18:11:35 -0500

    fix kernel tests for laplace and helmholtz

--- tests/kernel_helmholtz.cpp
+++ tests/kernel_helmholtz.cpp
@@ -1,32 +1,16 @@
 #include <numeric>
-#if HELMHOLTZ
 #include "helmholtz.h"
-#include "precompute_helmholtz.h"
-#else
-#include "laplace.h"
-#include "precompute_laplace.h"
-#endif
 #include "exafmm_t.h"
 #include "timer.h"
-
-namespace exafmm_t {
-  int P;
-  int NSURF;
-  int MAXLEVEL;
-  vec3 X0;
-  real_t R0;
-#if HELMHOLTZ
-  real_t WAVEK;
-#endif
-}
 
 using namespace exafmm_t;
 
 // Helper function to build the tree needed in kernel test
-void set_children(Node* parent, Node* first_child) {
+template <typename T>
+void set_children(Node<T>* parent, Node<T>* first_child) {
   parent->is_leaf = false;
   for(int octant=0; octant<8; ++octant) {
-    Node* child = first_child + octant;
+    Node<T>* child = first_child + octant;
     child->octant = octant;
     child->parent = parent;
     child->level = parent->level + 1;
@@ -44,40 +28,39 @@ int main(int argc, char **argv) {
 #if HAVE_OPENMP
   omp_set_num_threads(args.threads);
 #endif
-  // set global variables
-  P = args.P;
-  NSURF = 6*(P-1)*(P-1) + 2;
-  MAXLEVEL = 3;
-  X0 = 4.;
-  R0 = 4.;
-#if HELMHOLTZ
-  WAVEK = args.k;
-#endif
+
+  // create fmm instance
+  HelmholtzFMM fmm;
+  fmm.ncrit = args.ncrit;
+  fmm.p = args.P;
+  fmm.nsurf = 6*(fmm.p-1)*(fmm.p-1) + 2;
+  fmm.depth = 3;
+  fmm.x0 = 4.;
+  fmm.r0 = 4.;
+  fmm.wavek = args.k;
+
   // precomputation
   init_rel_coord();
-  precompute();
+  fmm.precompute();
 
   // create tree
   std::vector<int> nnodes = {1, 8, 8*2, 8*2};  // number of nodes at each level
-  Nodes nodes(std::accumulate(nnodes.begin(), nnodes.end(), 0));
+  Nodes<complex_t> nodes(std::accumulate(nnodes.begin(), nnodes.end(), 0));
+
   // initialize nodes
   for(size_t i=0; i<nodes.size(); ++i) {
-    Node& node = nodes[i];
+    Node<complex_t>& node = nodes[i];
     node.is_leaf = true;
     node.idx = i;
-#if COMPLEX
-    node.up_equiv.resize(NSURF, complex_t(0.,0.));
-    node.dn_equiv.resize(NSURF, complex_t(0.,0.));
-#else
-    node.up_equiv.resize(NSURF, 0.);
-    node.dn_equiv.resize(NSURF, 0.);
-#endif
+    node.up_equiv.resize(fmm.nsurf, 0.);
+    node.dn_equiv.resize(fmm.nsurf, 0.);
   }
+
   // set root node
-  Node* root = &nodes[0];
+  Node<complex_t>* root = &nodes[0];
   root->parent = nullptr;
-  root->x = X0;
-  root->r = R0;
+  root->x = fmm.x0;
+  root->r = fmm.r0;
   root->level = 0;
 
   // create descendants
@@ -88,33 +71,37 @@ int main(int argc, char **argv) {
   set_children(&nodes[24], &nodes[33]); // lvl 3 nodes
 
   // add source and target
-  Node* source = &nodes[25];   // lvl 3 source node
-  Node* target = &nodes[40];   // lvl 3 target node
+  Node<complex_t>* source = &nodes[25];   // lvl 3 source node
+  Node<complex_t>* target = &nodes[40];   // lvl 3 target node
   source->src_coord.resize(3, 0.5);
   source->src_value.push_back(1.0);
   target->trg_coord.resize(3, 7.5);
   target->trg_value.resize(4, 0.);
   
 #if DEBUG
-  cout << "index level is_leaf nsrcs ntrgs" << endl;
+  std::cout << "index level is_leaf nsrcs ntrgs" << std::endl;
   for(auto& node : nodes) {
-    cout << node.idx << " " << node.level << " " << node.is_leaf << " "
-         << node.src_coord.size()/3 << " " << node.trg_coord.size()/3 <<endl;
+    std::cout << node.idx << " " << node.level << " " << node.is_leaf << " "
+              << node.src_coord.size()/3 << " " << node.trg_coord.size()/3 << std::endl;
   }
-  return 0;
 #endif
 
   // P2M
-  NodePtrs leafs;
+  NodePtrs<complex_t> leafs;
   leafs.push_back(source);
-  P2M(leafs);
+  fmm.P2M(leafs);
+#if DEBUG
+  std::cout << "lvl 3 source node's upward equivalent charges" << std::endl;
+  for(int i=0; i<fmm.nsurf; ++i)
+    std::cout << i << " " << source->up_equiv[i] << std::endl;
+#endif
 
   // M2M
-  M2M(root);
+  fmm.M2M(root);
 #if DEBUG
-  cout << "lvl 2 source node's upward equivalent charges" << endl;
-  for(int i=0; i<NSURF; ++i) {
-    cout << i << " " << source->parent->up_equiv[i] << endl;
+  std::cout << "lvl 2 source node's upward equivalent charges" << std::endl;
+  for(int i=0; i<fmm.nsurf; ++i) {
+    std::cout << i << " " << source->parent->up_equiv[i] << std::endl;
   }
 #endif
 
@@ -122,33 +109,25 @@ int main(int argc, char **argv) {
   target->parent->parent->M2L_list.resize(REL_COORD[M2L_Type].size(), nullptr);
   target->parent->parent->M2L_list[0] = source->parent->parent;
   // M2L
-  NodePtrs nonleafs;
+  NodePtrs<complex_t> nonleafs;
   nonleafs.push_back(target->parent->parent);
-  M2L_setup(nonleafs);
-  M2L(nodes);
+  fmm.M2L_setup(nonleafs);
+  fmm.M2L(nodes);
 
   // L2L
-  L2L(root);
+  fmm.L2L(root);
 
   // L2P
   leafs.clear();
   leafs.push_back(target);
-  L2P(leafs);
+  fmm.L2P(leafs);
 
   // direct summation
-#if HELMHOLTZ
   ComplexVec trg_value_direct(4, 0.);
-#else
-  RealVec trg_value_direct(4, 0.);
-#endif
-  gradient_P2P(source->src_coord, source->src_value, target->trg_coord, trg_value_direct);
+  fmm.gradient_P2P(source->src_coord, source->src_value, target->trg_coord, trg_value_direct);
 
   // calculate error
-#if HELMHOLTZ
   ComplexVec& trg_value = target->trg_value;
-#else
-  RealVec& trg_value = target->trg_value;
-#endif
   real_t p_diff = 0, p_norm = 0, p_error = 0;
   p_diff = std::norm(trg_value[0]-trg_value_direct[0]);
   p_norm = std::norm(trg_value_direct[0]);
---
+++ tests/kernel_laplace.cpp
@@ -0,0 +1,144 @@
+#include <numeric>
+#include "laplace.h"
+#include "exafmm_t.h"
+#include "timer.h"
+
+using namespace exafmm_t;
+
+// Helper function to build the tree needed in kernel test
+template <typename T>
+void set_children(Node<T>* parent, Node<T>* first_child) {
+  parent->is_leaf = false;
+  for(int octant=0; octant<8; ++octant) {
+    Node<T>* child = first_child + octant;
+    child->octant = octant;
+    child->parent = parent;
+    child->level = parent->level + 1;
+    child->x = parent->x;
+    child->r = parent->r / 2;
+    for(int d=0; d<3; d++) {
+      child->x[d] += child->r * (((octant & 1 << d) >> d) * 2 - 1);
+    }
+    parent->children.push_back(child);
+  }
+}
+
+int main(int argc, char **argv) {
+  Args args(argc, argv);
+#if HAVE_OPENMP
+  omp_set_num_threads(args.threads);
+#endif
+
+  // create fmm instance
+  LaplaceFMM fmm;
+  fmm.ncrit = args.ncrit;
+  fmm.p = args.P;
+  fmm.nsurf = 6*(fmm.p-1)*(fmm.p-1) + 2;
+  fmm.depth = 3;
+  fmm.x0 = 4.;
+  fmm.r0 = 4.;
+
+  // precomputation
+  init_rel_coord();
+  fmm.precompute();
+
+  // create tree
+  std::vector<int> nnodes = {1, 8, 8*2, 8*2};  // number of nodes at each level
+  Nodes<real_t> nodes(std::accumulate(nnodes.begin(), nnodes.end(), 0));
+
+  // initialize nodes
+  for(size_t i=0; i<nodes.size(); ++i) {
+    Node<real_t>& node = nodes[i];
+    node.is_leaf = true;
+    node.idx = i;
+    node.up_equiv.resize(fmm.nsurf, 0.);
+    node.dn_equiv.resize(fmm.nsurf, 0.);
+  }
+
+  // set root node
+  Node<real_t>* root = &nodes[0];
+  root->parent = nullptr;
+  root->x = fmm.x0;
+  root->r = fmm.r0;
+  root->level = 0;
+
+  // create descendants
+  set_children(root, &nodes[1]);      // lvl 1 nodes
+  set_children(&nodes[1], &nodes[9]);  // lvl 2 nodes left corner
+  set_children(&nodes[8], &nodes[17]);  // lvl 2 nodes upper corner
+  set_children(&nodes[9], &nodes[25]);  // lvl 3 nodes
+  set_children(&nodes[24], &nodes[33]); // lvl 3 nodes
+
+  // add source and target
+  Node<real_t>* source = &nodes[25];   // lvl 3 source node
+  Node<real_t>* target = &nodes[40];   // lvl 3 target node
+  source->src_coord.resize(3, 0.5);
+  source->src_value.push_back(1.0);
+  target->trg_coord.resize(3, 7.5);
+  target->trg_value.resize(4, 0.);
+  
+#if DEBUG
+  std::cout << "index level is_leaf nsrcs ntrgs" << std::endl;
+  for(auto& node : nodes) {
+    std::cout << node.idx << " " << node.level << " " << node.is_leaf << " "
+              << node.src_coord.size()/3 << " " << node.trg_coord.size()/3 << std::endl;
+  }
+#endif
+
+  // P2M
+  NodePtrs<real_t> leafs;
+  leafs.push_back(source);
+  fmm.P2M(leafs);
+#if DEBUG
+  std::cout << "lvl 3 source node's upward equivalent charges" << std::endl;
+  for(int i=0; i<fmm.nsurf; ++i)
+    std::cout << i << " " << source->up_equiv[i] << std::endl;
+#endif
+
+  // M2M
+  fmm.M2M(root);
+#if DEBUG
+  std::cout << "lvl 2 source node's upward equivalent charges" << std::endl;
+  for(int i=0; i<fmm.nsurf; ++i) {
+    std::cout << i << " " << source->parent->up_equiv[i] << std::endl;
+  }
+#endif
+
+  // set up M2L_list
+  target->parent->parent->M2L_list.resize(REL_COORD[M2L_Type].size(), nullptr);
+  target->parent->parent->M2L_list[0] = source->parent->parent;
+  // M2L
+  NodePtrs<real_t> nonleafs;
+  nonleafs.push_back(target->parent->parent);
+  fmm.M2L_setup(nonleafs);
+  fmm.M2L(nodes);
+
+  // L2L
+  fmm.L2L(root);
+
+  // L2P
+  leafs.clear();
+  leafs.push_back(target);
+  fmm.L2P(leafs);
+
+  // direct summation
+  RealVec trg_value_direct(4, 0.);
+  fmm.gradient_P2P(source->src_coord, source->src_value, target->trg_coord, trg_value_direct);
+
+  // calculate error
+  RealVec& trg_value = target->trg_value;
+  real_t p_diff = 0, p_norm = 0, p_error = 0;
+  p_diff = std::norm(trg_value[0]-trg_value_direct[0]);
+  p_norm = std::norm(trg_value_direct[0]);
+  p_error = sqrt(p_diff/p_norm);
+  real_t F_diff = 0, F_norm = 0, F_error = 0;
+  for(int d=1; d<4; ++d) {
+    F_diff += std::norm(trg_value[d]-trg_value_direct[d]);
+    F_norm += std::norm(trg_value_direct[d]);
+  }
+  F_error = sqrt(F_diff/F_norm);
+  print("Potential Error", p_error);
+  print("Gradient Error", F_error);
+
+  return 0;
+}
