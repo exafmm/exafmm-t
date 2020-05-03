c65632e4b1600ec47a68a2cd5a629f0094e6bae6
Tingyu Wang <tingyu.wang6@gmail.com>
2020-05-02 22:45:26 -0400

    use adaptive tree in Python module

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
         'msvc': ['/EHsc'],
-        'unix': ['-fopenmp', '-DNON_ADAPTIVE', '-march=native', '-O2'],
+        'unix': ['-fopenmp', '-march=native', '-O2'],
     }
     l_opts = {
         'msvc': [],
