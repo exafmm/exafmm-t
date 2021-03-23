c2529e4f6005f6b3f7aa442ac3705705b421c8e1
Tingyu Wang <tingyu.wang6@gmail.com>
2021-03-22 21:47:44 -0400

    add copyright notice to intrinsics.h

--- include/intrinsics.h
+++ include/intrinsics.h
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2007 Dhairya Malhotra
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Used under written permission from the author.
+ */
+
 #ifndef intrinsics_h
 #define intrinsics_h
 
